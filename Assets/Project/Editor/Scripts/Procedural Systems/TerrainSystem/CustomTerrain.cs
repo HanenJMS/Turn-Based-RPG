b3bb68508731e093bd9e1c8e26f34edfebb58934
HanenJMS <Hanensgamelab@gmail.com>
2023-05-15 16:14:27 -0400

    refactoring code again. adding a CustomTerrainEditor class, and imported packages for GUI editor Table from the asset store.

--- Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/CustomTerrain.cs
+++ Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/CustomTerrain.cs
@@ -1,12 +1,44 @@
-using System.Collections;
+using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace HanensGameLab.Utilities.ProceduralSystems.TerrainSystem
 {
+    [ExecuteInEditMode]
     public class CustomTerrain : MonoBehaviour
     {
+        [SerializeField] List<string> terrainTags = new List<string>();
+        private void Awake()
+        {
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
 
+            SerializedProperty editorInspectorTagProperty = tagManager.FindProperty("tags");
+
+            AddTag(editorInspectorTagProperty, "Terrain");
+            AddTag(editorInspectorTagProperty, "Cloud");
+            AddTag(editorInspectorTagProperty, "Shore");
+
+            tagManager.ApplyModifiedProperties();
+
+            //foreach (string tagName in terrainTags)
+            //{
+            //    AddTag(editorInspectorTagProperty, tagName);
+            //}
+
+            this.gameObject.tag = "Terrain";
+        }
+
+        private void AddTag(SerializedProperty editorInspectorTagProperty, string newTag)
+        {
+            for (int i = 0; i < editorInspectorTagProperty.arraySize; i++)
+            {
+                string inspectorTagPropertyString = editorInspectorTagProperty.GetArrayElementAtIndex(i).stringValue;
+                if (inspectorTagPropertyString.Equals(newTag)) return;
+            }
+
+            editorInspectorTagProperty.InsertArrayElementAtIndex(0);
+            editorInspectorTagProperty.GetArrayElementAtIndex(0).stringValue = newTag;
+        }
     }
 }
 
---
+++ Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,16 @@
+using HanensGameLab.Utilities.ProceduralSystems.TerrainSystem;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor
+{
+    private void OnEnable()
+    {
+
+    }
+    public override void OnInspectorGUI()
+    {
+
+    }
+}
