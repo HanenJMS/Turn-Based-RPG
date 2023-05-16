e8318748f2359e2f39efb0011f2e5ce3a19b4532
HanenJMS <Hanensgamelab@gmail.com>
2023-05-16 01:18:19 -0400

    Procedural Terrain CustomTerrainEditor and Custom Terrain editor. Able to set height maps using PNG and set random values.

--- Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/CustomTerrain.cs
+++ Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/CustomTerrain.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
+using TreeEditor;
 
 namespace HanensGameLab.Utilities.ProceduralSystems.TerrainSystem
 {
@@ -8,6 +9,19 @@ namespace HanensGameLab.Utilities.ProceduralSystems.TerrainSystem
     public class CustomTerrain : MonoBehaviour
     {
         [SerializeField] List<string> terrainTags = new List<string>();
+        [SerializeField] Vector2 randomHeightRange = new Vector2(0, 0.1f);
+
+
+        //does this break MVC architecture? what are the responsibilities of these properties?
+        [SerializeField] Texture2D heightMapImage;
+        [SerializeField] Vector3 heightMapScale = new Vector3(1,1,1);
+
+        //Perlin Noise
+        [SerializeField] float perlinScaleX = 0.01f;
+        [SerializeField] float perlinScaleY = 0.01f;
+
+        [SerializeField] Terrain terrain;
+        [SerializeField] TerrainData terrainData;
         private void Awake()
         {
             SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
@@ -27,7 +41,68 @@ namespace HanensGameLab.Utilities.ProceduralSystems.TerrainSystem
 
             this.gameObject.tag = "Terrain";
         }
+        private void OnEnable()
+        {
+            Debug.Log("Initializing Terrain Data");
+            terrain = this.GetComponent<Terrain>();
+            terrainData = terrain.terrainData;
+            
+        }
+        private void PerlinFunction()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void GenerateTerrainHeight(bool ResetHeightMap)
+        {
+            float[,] terrainHeightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+            for (int x = 0; x < terrainData.heightmapResolution; x++)
+            {
+                for (int z = 0; z < terrainData.heightmapResolution; z++)
+                {
+                    if (ResetHeightMap) terrainHeightMap[x, z] = 0;
+                    else
+                    {
+                        terrainHeightMap[x, z] += Random.Range(randomHeightRange.x, randomHeightRange.y);
+                    }
+                }
+            }
+            terrainData.SetHeights(0, 0, terrainHeightMap);
+        }
 
+
+
+        public void LoadHeightMapTexture()
+        {
+            float[,] terrainHeightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];        
+            for (int x = 0; x < terrainData.heightmapResolution; x++)
+            {
+                for (int z = 0; z < terrainData.heightmapResolution; z++)
+                {
+                    terrainHeightMap[x, z] = heightMapImage.GetPixel((int)(x * heightMapScale.x), (int)(z * heightMapScale.z)).grayscale * heightMapScale.y;
+                }
+            }
+            terrainData.SetHeights(0, 0, terrainHeightMap);
+        }
+        //void RandomTerrain(float[,] terrainHeightMap)
+        //{
+        //    for(int x = 0; x < terrainData.heightmapResolution; x++)
+        //    {
+        //        for(int z = 0; z < terrainData.heightmapResolution; z++)
+        //        {
+        //            terrainHeightMap[x, z] += Random.Range(randomHeightRange.x, randomHeightRange.y); 
+        //        }
+        //    }
+        //}
+        //void ResetTerrain(float[,] terrainHeightMap)
+        //{
+        //    for (int x = 0; x < terrainData.heightmapResolution; x++)
+        //    {
+        //        for (int z = 0; z < terrainData.heightmapResolution; z++)
+        //        {
+        //            terrainHeightMap[x, z] = 0;
+        //        }
+        //    }
+        //}
         private void AddTag(SerializedProperty editorInspectorTagProperty, string newTag)
         {
             for (int i = 0; i < editorInspectorTagProperty.arraySize; i++)
--- Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/Editor/CustomTerrainEditor.cs
+++ Assets/Project/Editor/Scripts/Procedural Systems/TerrainSystem/Editor/CustomTerrainEditor.cs
@@ -1,16 +1,77 @@
 using HanensGameLab.Utilities.ProceduralSystems.TerrainSystem;
+using System;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(CustomTerrain))]
 [CanEditMultipleObjects]
 public class CustomTerrainEditor : Editor
 {
+    //properties ------------------
+    SerializedProperty customTerrainRandomHeightRange;
+    SerializedProperty customTerrainHeightMapScale;
+    SerializedProperty customTerrainheightMapImage;
+
+    //fold outs -------------------
+    bool showRandom = false;
+    bool showLoadHeights = false;
     private void OnEnable()
     {
-
+        customTerrainRandomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        customTerrainHeightMapScale = serializedObject.FindProperty("heightMapImage");
+        customTerrainheightMapImage = serializedObject.FindProperty("heightMapScale");
+        if (customTerrainRandomHeightRange == null)
+        {
+            Debug.Log($"{this.name} is missing a serializedProperty. FindProperty() found nothing. Maybe don't use strings?");
+        }
     }
     public override void OnInspectorGUI()
     {
+        serializedObject.Update();
 
+        CustomTerrain terrain = (CustomTerrain)target;
+        HandleRandomTerrainHeightGeneration(terrain);
+        HandleLoadingTerrainHeight(terrain);
+        HandleResetTerrain(terrain);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+    private void HandleRandomTerrainHeightGeneration(CustomTerrain terrain)
+    {
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random Height Generator");
+        
+        if (showRandom)
+        {
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(customTerrainRandomHeightRange);
+            if (GUILayout.Button("Random Heights"))
+            {
+                terrain.GenerateTerrainHeight(false);
+            }
+        }
+    }
+    private void HandleLoadingTerrainHeight(CustomTerrain terrain)
+    {
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Load Heights");
+        if (showLoadHeights)
+        { 
+            GUILayout.Label("Load Terrain Heights From Texture", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(customTerrainHeightMapScale);
+            EditorGUILayout.PropertyField(customTerrainheightMapImage);
+            if (GUILayout.Button("Load Texture"))
+            {
+                terrain.LoadHeightMapTexture();
+            }
+        }
+    }
+    private static void HandleResetTerrain(CustomTerrain terrain)
+    {
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Reset Height to Zero"))
+        {
+            terrain.GenerateTerrainHeight(true);
+        }
     }
 }
