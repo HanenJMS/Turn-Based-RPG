baa519ad6b51c3b002cb17b09ca1a79c04913a45
HanenJMS <Hanensgamelab@gmail.com>
2024-01-07 20:18:38 -0500

    adding building interactions

--- Assets/Project/Runtime/Scripts/GridSystem/GridBuildingSystem/Building.cs
+++ Assets/Project/Runtime/Scripts/GridSystem/GridBuildingSystem/Building.cs
@@ -6,33 +6,7 @@ namespace RPGSandBox.GameUtilities.GridBuildingSystems
 {
     public class Building : InteractableWorldObject, IAmInteractable
     {
-        [SerializeField] string descriptionHeader;
-        [TextArea(5,20)]
-        [SerializeField] string descriptionContent;
-        public bool CanInteract(IAmInteractable interact)
-        {
-            return true;
-        }
 
-        public string DescriptionContent()
-        {
-            return descriptionContent;
-        }
-
-        public string DescriptionHeader()
-        {
-            return descriptionHeader;
-        }
-
-        public Vector3 GetWorldPosition()
-        {
-            return this.transform.position;
-        }
-
-        public string InteractableName()
-        {
-            return descriptionHeader;
-        }
     }
 }
 
